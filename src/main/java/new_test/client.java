89ceea862f99f50002e43599ba09d3b1d50b64c7
juzi <hanks0407@163.com>
2018-09-25 16:52:45 +0800

    读取文件并传输 ，大功告成！！！！！！！！！！

--- src/main/java/new_test/client.java
+++ src/main/java/new_test/client.java
@@ -39,29 +39,50 @@ public class client {
                 System.out.println(header);
                 String line = null;
                 int j = 0;
-                while((line=reader.readLine())!=null){
+                String sent_message;
+                String sent_valid_message = "TRUE";
+                while((line=reader.readLine())!=null && (income_message = din.readUTF()) != null){
                     j++;
                     String [] item = line.split("，");//CSV格式文件为逗号分隔符文件，这里根据逗号切分
                     String last = item[item.length-1];//这就是你要的数据了
-
                     String[] last_ = last.split(",");
-//                    ((Number)last_[1]).floatValue();
-                    System.out.println("cust_id："+last_[0]);
-                    System.out.println("TX_score_WOE："+Float.valueOf(last_[1].trim()).floatValue());
-                    System.out.println("TD_Fraud_score_WOE："+Float.valueOf(last_[2].trim()).floatValue());
-                    System.out.println("C_city_x_encoding_WOE："+Float.valueOf(last_[3].trim()).floatValue());
-                    System.out.println("GEO_SCORE_geo104_WOE："+Float.valueOf(last_[4].trim()).floatValue());
-                    System.out.println("huomou_score_WOE："+Float.valueOf(last_[5].trim()).floatValue());
-                    System.out.println("sex_age_encoding_WOE："+Float.valueOf(last_[6].trim()).floatValue());
-                    System.out.println("match_score_mz_WOE："+Float.valueOf(last_[7].trim()).floatValue());
-                    System.out.println("auth_contactnum_ratio_30d_mz_WOE："+Float.valueOf(last_[8].trim()).floatValue());
-                    System.out.println("CDZC003_geo104_WOE："+Float.valueOf(last_[9].trim()).floatValue());
-                    System.out.println("brand_x_encoding_WOE："+Float.valueOf(last_[10].trim()).floatValue());
-                    System.out.println("C_app_date_WOE："+Float.valueOf(last_[11].trim()).floatValue());
-                    System.out.println("idcard_name_in_gray_mz_WOE："+Float.valueOf(last_[12].trim()).floatValue());
-                    System.out.println("intercept："+Float.valueOf(last_[13].trim()).floatValue());
 
-                    System.out.println("dijihang: "+j);
+                    Map featureNames = new HashMap();
+
+                    featureNames.put("cust_id：",last_[0]);
+                    featureNames.put("TX_score_WOE：",Float.valueOf(last_[1].trim()).floatValue());
+                    featureNames.put("TD_Fraud_score_WOE：",Float.valueOf(last_[2].trim()).floatValue());
+                    featureNames.put("C_city_x_encoding_WOE：",Float.valueOf(last_[3].trim()).floatValue());
+                    featureNames.put("GEO_SCORE_geo104_WOE：",Float.valueOf(last_[4].trim()).floatValue());
+                    featureNames.put("huomou_score_WOE：",Float.valueOf(last_[5].trim()).floatValue());
+                    featureNames.put("sex_age_encoding_WOE：",Float.valueOf(last_[6].trim()).floatValue());
+                    featureNames.put("match_score_mz_WOE：",Float.valueOf(last_[7].trim()).floatValue());
+                    featureNames.put("auth_contactnum_ratio_30d_mz_WOE：",Float.valueOf(last_[8].trim()).floatValue());
+                    featureNames.put("CDZC003_geo104_WOE：",Float.valueOf(last_[9].trim()).floatValue());
+                    featureNames.put("brand_x_encoding_WOE：",Float.valueOf(last_[10].trim()).floatValue());
+                    featureNames.put("C_app_date_WOE：",Float.valueOf(last_[11].trim()).floatValue());
+                    featureNames.put("idcard_name_in_gray_mz_WOE：",Float.valueOf(last_[12].trim()).floatValue());
+                    featureNames.put("intercept：",Float.valueOf(last_[13].trim()).floatValue());
+
+                    System.out.println(featureNames.toString());
+
+                    System.out.println("接受到新信息： "+ income_message);
+
+                    sent_message = featureNames.toString();
+                    dout.writeUTF(sent_message);
+                    dout.flush();
+                    System.out.println("发送了新数据："+sent_message);
+
+                    if(j>90){
+                        sent_valid_message = "FALSE";
+                        dout.writeUTF(sent_valid_message);
+                        dout.flush();
+                    }else{
+                        sent_valid_message = "TRUE";
+                        dout.writeUTF(sent_valid_message);
+                        dout.flush();
+                    }
+
                     }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -69,30 +90,6 @@ public class client {
 
 //            dout.writeUTF("我准备开始发送数据了！");
 //            dout.flush();
-
-            String sent_message;
-            String sent_valid_message = "TRUE";
-            int i = 0;
-            while ((income_message = din.readUTF()) != null) {
-
-                System.out.println("接受到新信息： "+income_message);
-
-                sent_message = ("{cust_id:a1234,name:'小强',age:23}"+i).toString();
-                dout.writeUTF(sent_message);
-                dout.flush();
-                System.out.println("发送了新数据："+sent_message);
-
-                if(i>10){
-                    sent_valid_message = "FALSE";
-                    dout.writeUTF(sent_valid_message);
-                    dout.flush();
-                }else{
-                    sent_valid_message = "TRUE";
-                    dout.writeUTF(sent_valid_message);
-                    dout.flush();
-                }
-                i++;
-            }
 
             dout.flush();
             dout.close();
